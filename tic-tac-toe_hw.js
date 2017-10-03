b84d7ac9a726f607366de868f78f98142831ca43
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-03 10:06:01 -0400

    Tic-tac-toe

--- tic-tac-toe_hw.js
+++ tic-tac-toe_hw.js
@@ -146,7 +146,7 @@ function fillBoard(sideLength){
 	for (let i = 0; i < sideLength; i++){
 		playingBoard += '<div class="board-row">';
 		for (let j = 0; j < sideLength; j++){
-			playingBoard +=`<button id="${j}${i}" class="square" style='width:${100/sideLength}%; height:${100/sideLength}%'>-</button>`;
+			playingBoard +=`<button id="${j}${i}" class="square" style='width:${185*3/sideLength}px; height:${185*3/sideLength}px'>-</button>`;
 		}
 		playingBoard += '</div>';
 	}
