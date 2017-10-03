77538c3f072b9ba47051c72d2dc4576d61eaaeb5
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-03 10:26:44 -0400

    Tic-tac-toe

--- board-size.js
+++ board-size.js
@@ -1,12 +1,13 @@
 var playingBoard = "";
+
 function fillBoard(sideLength){
 	for (let i = 0; i < sideLength; i++){
 		playingBoard += '<div class="board-row">';
 		for (let j = 0; j < sideLength; j++){
-			playingBoard +=`<button id="${j}${i}" class="square" style='width:${100/sideLength}%; height:${100/sideLength}%'>-</button>`;
+			playingBoard +=`<button id="${j}${i}" class="square" style='width:${185*3/sideLength}px; height:${185*3/sideLength}px'>-</button>`;
 		}
 		playingBoard += '</div>';
 	}
 	return playingBoard;
 }
-console.log(fillBoard(3));
+console.log(fillBoard(3)); 
